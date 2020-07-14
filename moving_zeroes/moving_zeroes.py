10ac9d7f219692b6cffaf375a9f24e52c1eeceea
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2020-07-14 14:29:35 -0400

    Module 3 - moving_zeroes complete

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -3,9 +3,21 @@ Input: a List of integers
 Returns: a List of integers
 '''
 def moving_zeroes(arr):
-    # Your code here
+    # 0s need to be on the right
+    # all other nums on the left
 
-    pass
+    # Create two arrays - one for 0s and one for other nums
+    new_arr = []
+    zero_arr = []
+
+    # loop through array
+    for num in arr:
+        if num == 0:
+            zero_arr.append(num)
+        else:
+            new_arr.append(num)
+    # return arr1 + arr2
+    return new_arr + zero_arr
 
 
 if __name__ == '__main__':
