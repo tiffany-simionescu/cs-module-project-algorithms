95bea562dcf0f08b498a21370361b134e0a50281
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2020-07-14 14:05:57 -0400

    Module 3 - single_number complete

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -3,9 +3,21 @@ Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
 def single_number(arr):
-    # Your code here
+    # There will be a minimum len(arr) == 3
+    if len(arr) < 3:
+        return -1
 
-    pass
+    single_int = []
+    # we want to loop over the arr and see if num is contained in arr twice
+    for num in arr:
+        # Add numbers to list
+        if num not in single_int:
+            single_int.append(num)
+        # if the num exsists in the list, than remove the number already in the list
+        else:
+            single_int.remove(num)
+
+    return single_int.pop()
 
 
 if __name__ == '__main__':
@@ -13,3 +25,4 @@ if __name__ == '__main__':
     arr = [1, 1, 4, 4, 5, 5, 3, 3, 9, 0, 0]
 
     print(f"The odd-number-out is {single_number(arr)}")
+    
