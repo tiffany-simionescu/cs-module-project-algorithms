8a057f1178e96289047aef45ba79c06ff0acc3fe
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2020-07-14 22:36:23 -0400

    Moduel 3 - sliding_window_max complete

--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -3,9 +3,29 @@ Input: a List of integers as well as an integer `k` representing the size of the
 Returns: a List of integers
 '''
 def sliding_window_max(nums, k):
-    # Your code here
+    # k is the size of the window/list
+    # it moves right by 1 index and will hold k amounts of indexes
+    # find the greater number
+    # append the greater number to a new list
+    # if k is greater than the len of nums, return -1
 
-    pass
+    new_arr = []
+    start = 0
+    end = k
+
+    if end > len(nums):
+        return -1
+
+    while end <= len(nums):
+        greater_num = nums[start]
+        for i in range(start + 1, end):
+            if nums[i] > greater_num:
+                greater_num = nums[i]
+        new_arr.append(greater_num)
+        start += 1
+        end += 1
+
+    return new_arr
 
 
 if __name__ == '__main__':
