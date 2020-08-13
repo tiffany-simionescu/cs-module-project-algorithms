b7b1f6a06bf421136ba06a0fde08cce217c0b62f
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2020-08-12 23:08:55 -0400

    Updated eating_cookies

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,7 +2,7 @@
 Input: an integer
 Returns: an integer
 '''
-def eating_cookies(n):
+def eating_cookies_small(n):
     # There are n cookies in a jar
     # Cookie Monster can eat 1, 2, 3 cookies at a time
     # what are all the possibilities CM can eat the cookies?
@@ -18,31 +18,31 @@ def eating_cookies(n):
     elif n < 0:
         return 0
     else:
-        return eating_cookies(n - 3) + eating_cookies(n - 2) + eating_cookies(n - 1)
+        return eating_cookies_small(n - 3) + eating_cookies_small(n - 2) + eating_cookies_small(n - 1)
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
     num_cookies = 5
 
-    print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
+    print(f"There are {eating_cookies_small(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
 
 
 # For Larger Input
 
-# def eating_cookies(n, cache):
-#     if n == 0:
-#         return 1
-#     elif n < 0:
-#         return 0
-#     elif cache[n] > 0:
-#         return cache[n]
-#     else:
-#         cache[n] = eating_cookies(n - 1, cache) + eating_cookies(n - 2, cache) + eating_cookies(n - 3, cache)
-#         return cache[n]
+def eating_cookies_large(n, cache):
+    if n == 0:
+        return 1
+    elif n < 0:
+        return 0
+    elif cache[n] > 0:
+        return cache[n]
+    else:
+        cache[n] = eating_cookies_large(n - 1, cache) + eating_cookies_large(n - 2, cache) + eating_cookies_large(n - 3, cache)
+        return cache[n]
 
-# if __name__ == "__main__":
-#     # Use the main function here to test out your implementation
-#     num_cookies = 5
-#     cashe = [0 for i in range(6)]
+if __name__ == "__main__":
+    # Use the main function here to test out your implementation
+    num_cookies = 5
+    cache = [0 for i in range(6)]
 
-#     print(f"There are {eating_cookies(num_cookies, cache)} ways for Cookie Monster to each {num_cookies} cookies")
+    print(f"There are {eating_cookies_large(num_cookies, cache)} ways for Cookie Monster to each {num_cookies} cookies")
